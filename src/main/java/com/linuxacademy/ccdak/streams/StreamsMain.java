ba30f22656b2f77b6c17936aeb47cfedcb12c01a
srujan-at-122587656708 <dacharajusrujan@gmail.com>
2021-02-27 20:18:17 -0600

    kafka-strems

--- src/main/java/com/linuxacademy/ccdak/streams/StreamsMain.java
+++ src/main/java/com/linuxacademy/ccdak/streams/StreamsMain.java
@@ -1,9 +1,54 @@
 package com.linuxacademy.ccdak.streams;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
 public class StreamsMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Properties props = new Properties();
+        props.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "streams-exmple");
+        props.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0");
+        props.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> stream = builder.stream("streams-input-topic");
+
+        stream.to("streams-input-topic");
+
+        final Topology topology = builder.build(props);
+        final KafkaStreams streams = new KafkaStreams(topology, props);
+
+        System.out.println(topology.describe());
+
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-wordcount-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
+
+        try {
+            streams.start();
+            latch.await();
+        } catch (final Throwable ex) {
+            System.out.println(ex.getMessage());
+            System.exit(1);
+        }
+
+        System.exit(0);
     }
 
 }
